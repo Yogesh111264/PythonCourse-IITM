cf31f31c12128d0cfdae804d161cf34c491d400b
YOGESH KUMAR <>
2021-05-21 15:57:58 +0000

    Week3 GPA5 modified

--- Week3/GPA5.py
+++ Week3/GPA5.py
@@ -9,31 +9,19 @@ cond1 = n.isnumeric()==True and len(n)==10 and (n[0]=="6" or n[0]=="7" or n[0]==
 
 if cond1 == True :
   for num in n:
-    print(n.count(num))
     if n.count(num)>6:
       print("invalid") 
       break 
   else:
 
     #Number should not have any digit repeated more then 5 times in consecutive manner.
+    # AABBCC
 
-    for i in range(0,10):
-      counter =0 
-      for j in range(i,10):
-        if n[i] == n[j]:
-          counter += 1
-        else:
+    for i in range(0,5):
+      if n[i:i+5] == n[i]*5:
           break
-      if counter > 4 :
-        final = False
-        break
-      else:
-        final = True
-      
-    if final:
-      print("valid")
     else:
-      print("invalid")
+        print("valid")
 else:
   print("invalid")          
 
