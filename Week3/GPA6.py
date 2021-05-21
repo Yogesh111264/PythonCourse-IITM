7e5bc8913dbddf6f5091cebc52aeebeaf0f1a8ae
YOGESH KUMAR <>
2021-05-21 14:18:24 +0000

    few changed done and comment added.

--- Week3/GPA6.py
+++ Week3/GPA6.py
@@ -1,7 +1,11 @@
 n= input("")
 numerics ="0123456789"
 
+#Basic condtion Check 
+
 cond1 = n.isnumeric()==True and len(n)==10 and (n[0]=="6" or n[0]=="7" or n[0]=="8" or n[0]=="9" )
+
+#Number should not have any digit more than 7 times.
 
 if cond1 == True :
   for num in numerics:
@@ -13,6 +17,9 @@ if cond1 == True :
       print("invalid") 
       break 
   else:
+
+    #Number should not have any digit repeated more then 5 times in consecutive manner.
+
     for i in range(0,10):
       counter =0 
       for j in range(i,10):
